72065d9cbce136f6c2d32a6d118601ecbd35f473
Sam Hamm <samhamm@gmail.com>
2017-05-25 08:21:09 -0700

    prepped for class 5/25

--- public/app.js
+++ public/app.js
@@ -1,1 +1,23 @@
-'use strict';
+`use strict`;
+
+ function setColor(){
+   let colorVal = `#${$('form')[0].color.value}`;
+   $('#thebutton').css('background-color', colorVal)
+ }
+
+ function setRadius() {
+   let radVal = `${$('form')[0].radius.value}px`;
+   $('#thebutton').css('border-radius', radVal);
+ }
+
+ function setPhrase(){
+   let phraseVal = $('form')[0].phrase.value;
+   $('#thebutton').text(phraseVal);
+ }
+
+ $('form').on('change', function(){
+   setColor();
+   setRadius();
+   setPhrase();
+   $('#buttoncode').text(document.getElementById('thebutton').outerHTML);
+ })
