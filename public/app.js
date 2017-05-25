5a9153a98e5a927077e22c471981e88042ed5842
Izabella Baer <izabellabaer@gmail.com>
2017-05-25 13:26:24 -0700

    finished middleware demo

--- public/app.js
+++ public/app.js
@@ -1,23 +1,36 @@
 `use strict`;
 
- function setColor(){
-   let colorVal = `#${$('form')[0].color.value}`;
-   $('#thebutton').css('background-color', colorVal)
+ function setColor(ctx, next){
+   console.log(ctx);
+   $('#thebutton').css('background-color', `#${ctx.params.color}`);
+   $('form')[0].color.value = ctx.params.color;
+   next();
  }
 
- function setRadius() {
-   let radVal = `${$('form')[0].radius.value}px`;
-   $('#thebutton').css('border-radius', radVal);
+ function setRadius(ctx, next) {
+   $('#thebutton').css('border-radius', `${ctx.params.radius}px`);
+   $('form')[0].radius.value = ctx.params.radius;
+   next();
  }
 
- function setPhrase(){
-   let phraseVal = $('form')[0].phrase.value;
-   $('#thebutton').text(phraseVal);
+ function setPhrase(ctx){
+   $('#thebutton').text(ctx.params.phrase);
+   $('form')[0].phrase.value = ctx.params.phrase;
+   $('#buttoncode').text($('#thebutton')[0].outerHTML);
+  //  ^^ pushing that into the outerHTML on the page
  }
 
- $('form').on('change', function(){
-   setColor();
-   setRadius();
-   setPhrase();
-   $('#buttoncode').text(document.getElementById('thebutton').outerHTML);
- })
+// ctx.params.color
+ page('/:color/:radius/:phrase', setColor, setRadius, setPhrase);
+
+// an alternate way to write document.ready, its like a jquery IIFE, its our executable code that will be running on page load.
+$(function() {
+  const f = $('form')[0];
+  page();
+
+// event delegation - setting up event listeners that adds flexibility, when we add that second argument we are adding delegation that gives us flexibility
+  $('body').on('change', 'input', function() {
+    let path = [f.color.value, f.radius.value, f.phrase.value]..filter( v => v).join('/');
+    page.show(`/${path}`);
+  })
+})
